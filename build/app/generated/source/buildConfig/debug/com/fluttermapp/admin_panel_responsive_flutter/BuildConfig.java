5592ce0ca7c5c0d0816e13e2f8cde1e549a0f995
mrakylbek <akylbek.muratov.2001@mail.ru>
2023-01-11 00:51:16 +0600

    save

---
+++ build/app/generated/source/buildConfig/debug/com/fluttermapp/admin_panel_responsive_flutter/BuildConfig.java
@@ -0,0 +1,12 @@
+/**
+ * Automatically generated file. DO NOT MODIFY
+ */
+package com.fluttermapp.admin_panel_responsive_flutter;
+
+public final class BuildConfig {
+  public static final boolean DEBUG = Boolean.parseBoolean("true");
+  public static final String APPLICATION_ID = "com.fluttermapp.admin_panel_responsive_flutter";
+  public static final String BUILD_TYPE = "debug";
+  public static final int VERSION_CODE = 1;
+  public static final String VERSION_NAME = "1.0.0";
+}
